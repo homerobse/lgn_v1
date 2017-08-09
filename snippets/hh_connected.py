9da1529b6cf3e778e7fd7f47b71151645b332c0b
Homero Barrocas Soares Esmeraldo <homerobse@gmail.com>
2017-08-08 22:51:36 -0300

    Using original Hodgkin Huxley cells and plotting synaptic current

--- snippets/hh_connected.py
+++ snippets/hh_connected.py
@@ -9,62 +9,44 @@ print h.cas().name(), soma.name()
 soma2 = h.Section(name='soma2')
 print h.cas().name(), soma2.name()
 
-# soma.insert('hhvitor2')
-# soma.gna_hhvitor2 = 0.039
-# soma.gkd_hhvitor2 = 0.006
-# soma.gl_hhvitor2 = 0
-# soma.gm_hhvitor2 = 0
-# soma.gt_hhvitor2 = 0
-# soma.gleak_hhvitor2 = 0.0000273
-# soma2.insert('hhvitor2')
-# soma2.gna_hhvitor2 = 0.039
-# soma2.gkd_hhvitor2 = 0.006
-# soma2.gl_hhvitor2 = 0
-# soma2.gm_hhvitor2 = 0
-# soma2.gt_hhvitor2 = 0
-# soma2.gleak_hhvitor2 = 0.0000273
 soma.insert('hh_original')
 soma2.insert('hh_original')
 
-syn = h.ExpSyn(0.5, sec=soma2)
-# syn.e = 0
-# syn.tau1 = 1
-# syn.tau2 = 3
+syn = h.Exp2Syn(0.5, sec=soma2)
+syn.e = 0
+syn.tau1 = 1
+syn.tau2 = 3
 
-net_con = h.NetCon(soma(0.5)._ref_v, syn, 40, 5, 1)
+net_con = h.NetCon(soma(0.5)._ref_v, syn, 0, 5, 0.07)
 
 stim = h.IClamp(soma(0.5))
 stim.delay = 1
 stim.dur = 5
 stim.amp = 20
 
-stim2 = h.IClamp(soma2(0.5))
-stim2.delay = 20
-stim2.dur = 20
-stim2.amp = 20
-
 rec_cell = h.Vector()
 rec_cell.record(soma(0.5)._ref_v)
 rec_cell2 = h.Vector()
 rec_cell2.record(soma2(0.5)._ref_v)
+rec_syn = h.Vector()
+rec_syn.record(syn._ref_i)
 rec_stim = h.Vector()
 rec_stim.record(stim._ref_i)
-rec_stim2 = h.Vector()
-rec_stim2.record(stim2._ref_i)
-
 
 timeaxis = h.Vector()
 timeaxis.record(h._ref_t)
 
-print soma(0.5).v
-
-h.tstop = 50
+h.tstop = 80
 h.run()
 
-print soma(0.5).v
-
-plt.plot(timeaxis, rec_cell)
-plt.plot(timeaxis, rec_cell2)
-plt.plot(timeaxis, rec_stim)
-plt.plot(timeaxis, rec_stim2)
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].plot(timeaxis, rec_cell)
+axarr[0].plot(timeaxis, rec_cell2)
+axarr[0].plot(timeaxis, rec_stim)
+axarr[0].set_title("IClamp stimulus and voltage traces")
+axarr[0].set_ylabel('Voltage (mV)')
+axarr[1].plot(timeaxis, rec_syn)
+axarr[1].set_title("Synaptic current")
+axarr[1].set_ylabel("Current (mA)")
+plt.xlabel("Time (ms)")
 plt.show()
